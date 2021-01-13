fed2209fba8f4fb4168a02878ace512fd94881e0
Bobur62 <boburgoziyevmm62@gmail.com>
2021-01-13 22:03:10 +0500

    katta o'zgarishlar kiritildi

--- alltoys/settings.py
+++ alltoys/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/3.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
+# See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/4
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 't_lo42we(#o$r09b4-^8)+-zx-lk09tgi9fd!u20cko6dw7(z&'
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "toys"
+    "toys",
 ]
 
 MIDDLEWARE = [
@@ -76,8 +76,10 @@ WSGI_APPLICATION = 'alltoys.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': "alltoys",
+        "USER": "postgres",
+        "PASSWORD": "2389",
     }
 }
 
@@ -119,3 +121,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- alltoys/wsgi.py
+++ alltoys/wsgi.py
@@ -8,9 +8,19 @@ https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
 import os
-
+from dj_static import Cling
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'alltoys.settings')
 
-application = get_wsgi_application()
+# application = get_wsgi_application()
+
+
+# import os
+# from django.core.wsgi import get_wsgi_application
+# from dj_static import Cling
+
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nirla.settings") #nirla is the name of the project
+
+
+application = Cling(get_wsgi_application())
